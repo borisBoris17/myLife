2fa0b32f4fe05db4f6b32ffbb5723e86f815dc44
Tucker <borisblade17@gmail.com>
2025-01-07 23:01:22 -0600

    first commit

---
+++ myLife/myLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  myLifeApp.swift
+//  myLife
+//
+//  Created by tucker bichsel on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct myLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
